28f8658fdeb3393db7ca28080536f8afb91e7103
d-suter <david.raul.suter@icloud.com>
2024-05-14 10:32:34 +0000

    new phone color

--- device-check.js
+++ device-check.js
@@ -9,12 +9,14 @@ function displayMobileWarning() {
     warningDiv.style.left = "0";
     warningDiv.style.width = "100%";
     warningDiv.style.height = "100%";
-    warningDiv.style.backgroundColor = "white";
+    warningDiv.style.backgroundColor = "rgba(255, 255, 255, 0.8)";
     warningDiv.style.color = "black";
     warningDiv.style.display = "flex";
     warningDiv.style.justifyContent = "center";
     warningDiv.style.alignItems = "center";
     warningDiv.style.zIndex = "9999";
+    warningDiv.style.fontSize = "2em";
+    warningDiv.style.fontFamily = "'Jolly Lodger', cursive";
     warningDiv.innerText = "Only Supports PC";
 
     document.body.appendChild(warningDiv);
